3ec803b39dc352c1ed724fb517b0dfba97a0a3c1
mediatrice8 <kampiremediatrice@gmail.com>
2019-10-20 17:17:00 +0200

    login form

--- instagram1/forms.py
+++ instagram1/forms.py
@@ -1,6 +1,16 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm, Textarea, IntegerField
 from .models import Image, Profile, Follow
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
 class NewImageForm(forms.ModelForm):
     class Meta:
         model = Image
