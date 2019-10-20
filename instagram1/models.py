68e2eab2eab2baf86094b46a82e2b972bb209eb8
mediatrice8 <kampiremediatrice@gmail.com>
2019-10-20 16:58:34 +0200

    model or table in database

--- instagram1/models.py
+++ instagram1/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
 class Image(models.Model):
@@ -8,3 +10,32 @@ class Image(models.Model):
     image_caption = models.TextField()
     profile = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.image_caption
+    
+    class Meta:
+        ordering = ['created_date']
+        
+class Profile(models.Model):
+    profilePhoto= models.ImageField(upload_to='profile/',null=True,blank=True)
+    bio = models.CharField(max_length=60,blank=True)
+    user = models.ForeignKey(User, related_name='Profile', null=True, on_delete=models.CASCADE,blank=True,)
+    name = models.CharField(blank=True, max_length=150)
+    # post = HTMLField()
+    def __str__(self):
+        return self.name
+    
+    @classmethod
+    def search_by_user(cls,search_term):
+        profiles = cls.objects.filter(title__icontains=search_term)
+        return profiles
+    
+    
+class Follow(models.Model):
+    follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='followers')
+
+    def __str__(self):
+        return f'{self.follower} Follow'
+    
