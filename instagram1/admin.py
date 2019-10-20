fa3c6eaa3f459f7dd9f3b2e6673e170ec0902b56
mediatrice8 <kampiremediatrice@gmail.com>
2019-10-20 16:38:58 +0200

    dashbord

--- instagram1/admin.py
+++ instagram1/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Image, Profile, Follow, Comment
 # Register your models here.
+
+admin.site.register(Image)
+admin.site.register(Profile)
+admin.site.register(Comment)
+admin.site.register(Follow)
