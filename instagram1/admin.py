2cb1935adbcb924f5759114030eafa02d1cf4b7d
mediatrice8 <kampiremediatrice@gmail.com>
2019-10-20 17:10:53 +0200

    change

--- instagram1/admin.py
+++ instagram1/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Image, Profile, Follow, Comment
+from .models import Image, Profile, Follow
 # Register your models here.
 
 admin.site.register(Image)
 admin.site.register(Profile)
-admin.site.register(Comment)
+# admin.site.register(Comment)
 admin.site.register(Follow)
--- instagram1/urls.py
+++ instagram1/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.models import User
+
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    # url('^$',views.welcome,name = 'welcome'),
+    url(r'^search/', views.search_users, name='search_users'),
+    url(r'^profile/(?P<username>[0-9]+)$', views.profile, name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
