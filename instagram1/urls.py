bccc106fb0720375b838993be5b69829f7faf045
mediatrice8 <kampiremediatrice@gmail.com>
2019-10-22 18:50:27 +0200

    url helps to display data

--- instagram1/urls.py
+++ instagram1/urls.py
@@ -1,15 +1,22 @@
-from django.conf.urls import url
+from django.urls import path, include
+
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.auth.models import User
 
-
-urlpatterns=[
-    # url('^$',views.welcome,name = 'welcome'),
-    url(r'^search/', views.search_users, name='search_users'),
-    url(r'^profile/(?P<username>[0-9]+)$', views.profile, name='profile'),
+urlpatterns = [
+    path('signup/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
+    path('', views.index, name='index'),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
+    path('post/<id>', views.post_comment, name='comment'),
+    # path('post/<id>/like', PostLikeToggle.as_view(), name='liked'),
+    # path('api/post/<id>/like', PostLikeAPIToggle.as_view(), name='liked-api'),
+    path('like', views.like_post, name='like_post'),
+    path('search/', views.search_profile, name='search_profile'),
+    path('unfollow/<to_unfollow>', views.unfollow, name='unfollow'),
+    path('follow/<to_follow>', views.follow, name='follow')
 ]
-
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    
+    
+    
+    
