5b7c55ccaf949133f768f4d34df38beae8749284
mediatrice8 <kampiremediatrice@gmail.com>
2019-10-25 01:05:34 +0200

    update

--- instagram/urls.py
+++ instagram/urls.py
@@ -14,15 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 
+from django.conf.urls import url,include
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('instagram1.urls'))
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+    url(r'^admin/', admin.site.urls),
+    url(r'^tinymce/', include('tinymce.urls')),
+    url(r'',include('instagram1.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
+]
